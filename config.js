feaad994cb7c8dc9e028c45db4ba431140f3a13f
lethutrang71 <86722384+lethutrang71@users.noreply.github.com>
2021-07-19 10:14:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Lộc à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim emmm lẻ loi
+    Đằng sau chữ yêu đấy là thương`,
     btnIntro: 'Thế cơ à :)))',
-    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anhhh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anhh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Trang ❤❤❤',
-    btnNo: 'Đánh cho giờ, muốn làm em gái mưa à?? :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ nhở <3',
+    btnNo: 'Đánh cho giờ, lươn vừa thôiii  :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anhhh lại thích emm nhỏo <3',
     btnReply: 'Gửi Trang <33333',
-    reply: 'Thích thì nhích thuiiii 😜😜',
-    mess: 'Ỏ, tớ biết màaaa 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tớ có kèo đi foodtour Hải Phòng cậu muốn đi cùng tớ ko??? 😂😂.',
+    reply: 'Thích cần lý do à, hỏi thừaaa 😜😜',
+    mess: 'Ỏoo, em biết màaaa 🥰. Iêu vãiii 😘😘',
+    messDesc: 'Nếu anh không phiền thì làm ny em nhée??? 😂😂.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+} 
